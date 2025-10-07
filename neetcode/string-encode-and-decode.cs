a1bf037dc109afd60b9c145b9852ac2cc2b5208c
farang <artemiavorskyi@gmail.com>
2025-10-07 19:48:31 +0200

    uninished good solution for string encode and decode

--- neetcode/string-encode-and-decode.cs
+++ neetcode/string-encode-and-decode.cs
@@ -1,5 +1,5 @@
 // unfinished, bad solution
-public class Solution
+public class Solution1
 {
     const char startDevider = '[';
     const char endDevider = ']';
@@ -60,5 +60,65 @@ public class Solution
             }
         }
         return result;
+    }
+}
+
+// good solution
+public class Solution2
+{
+    const char JoinElement = '#';
+    public string Encode(IList<string> strs)
+    {
+        var encoded = "";
+
+        for (var i = 0; i < strs.Count; i++)
+        {
+            encoded += $"{strs[i].Length}{JoinElement}{strs[i]}";
+        }
+
+        return encoded;
+    }
+
+    public List<string> Decode(string s)
+    {
+        var decoded = new List<string>();
+
+        if (s.Length == 0)
+        {
+            return decoded;
+        }
+
+        string lengthStr = "0";
+        int? length = null;
+        string toPush = "";
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == JoinElement)
+            {
+                length = int.Parse(lengthStr);
+                lengthStr = "0";
+                toPush = "";
+                continue;
+            }
+
+            if (length is null)
+            {
+                lengthStr += s[i];
+                continue;
+            }
+            else if (length == 0)
+            {
+                decoded.Add(toPush);
+                length = null;
+                continue;
+            }
+            else if (length > 0)
+            {
+                toPush += s[i];
+                length -= 1;
+                continue;
+            }
+        }
+        return decoded;
     }
 }
