bfafce8ba75be1517ff4d866f4b9e5ffbb4502c7
farang <artemiavorskyi@gmail.com>
2025-10-09 21:09:02 +0200

    unfinished solution for valid sudoku

---
+++ neetcode/valid-sudoku.cs
@@ -0,0 +1,73 @@
+// unfinished solution for valid sudoku
+public class Solution
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        var len = board.Length;
+
+        var matrixMap = new Dictionary<int, bool[]>();
+
+        for (var i = 0; i < len; i++)
+        {
+            matrixMap[i] = new bool[10];
+        }
+
+        for (var i = 0; i < len; i++)
+        {
+            var rowsMap = new Dictionary<int, bool>();
+            var colMap = new Dictionary<int, bool>();
+
+            for (var i1 = 0; i1 < len; i1++)
+            {
+                char rowNChar = board[i][i1];
+                char colNChar = board[i1][i];
+                // row
+                if (rowNChar != '.')
+                {
+                    var rn = rowNChar - '0';
+
+                    if (rowsMap.ContainsKey(rn))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        rowsMap[rn] = true;
+                    }
+                    var rowIndex1 = (int)Math.Floor((double)(i / 3));
+                    var columnIndex1 = (int)Math.Floor((double)(i1 / 3));
+                    var matrixIndex1 = (rowIndex1 * 3) + columnIndex1;
+                    if (matrixMap[matrixIndex1][rn] && i != i1)
+                    {
+                        return false;
+                    }
+                    matrixMap[matrixIndex1][rn] = true;
+                }
+                // column
+                if (colNChar == '.')
+                {
+                    continue;
+                }
+                var cn = colNChar - '0';
+
+                if (colMap.ContainsKey(cn))
+                {
+                    return false;
+                }
+                else
+                {
+                    colMap[cn] = true;
+                }
+                var rowIndex2 = (int)Math.Floor((double)(i1 / 3));
+                var columnIndex2 = (int)Math.Floor((double)(i / 3));
+                var matrixIndex2 = (rowIndex2 * 3) + columnIndex2;
+                // if (matrixMap[matrixIndex2][cn] && i != i1) {
+                //     return false;
+                // }
+                matrixMap[matrixIndex2][cn] = true;
+            }
+        }
+
+        return true;
+    }
+}
