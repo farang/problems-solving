4074169224ebbb4380cba8656827b2bcda29d69d
farang <artemiavorskyi@gmail.com>
2025-10-16 19:27:23 +0200

    two-integer-sum-ii (efficient solution)

--- neetcode/two-integer-sum-ii.cs
+++ neetcode/two-integer-sum-ii.cs
@@ -1,26 +1,36 @@
 // non-efficient solution
-public class Solution
-{
-    public int[] TwoSum(int[] numbers, int target)
-    {
-        for (var i = 0; i < numbers.Length; i++)
-        {
-            for (var i1 = 0; i1 < i; i1++)
-            {
-                if ((numbers[i] + numbers[i1]) == target)
-                {
-                    return new int[2] { i1 + 1, i + 1 };
-                }
-            }
-            for (var i1 = i + 1; i1 < numbers.Length; i1++)
-            {
-                if ((numbers[i] + numbers[i1]) == target)
-                {
-                    return new int[2] { i + 1, i1 + 1 };
+public class Solution1 {
+    public int[] TwoSum(int[] numbers, int target) {
+        for (var i = 0; i < numbers.Length; i++) {
+            for (var i1 = i + 1; i1 < numbers.Length; i1++) {
+                if ((numbers[i] + numbers[i1]) == target) {
+                    return new int[2]{ i + 1, i1 + 1 };
                 }
             }
         }
 
-        return new int[2] { 0, 1 };
+        return new int[2]{ 0, 1 };
+    }
+}
+
+// efficient solution
+public class Solution2 {
+    public int[] TwoSum(int[] numbers, int target) {
+        var lPointer = 0;
+        var rPointer = numbers.Length - 1;
+
+        while (lPointer < rPointer) {
+            int sum = numbers[lPointer] + numbers[rPointer];
+
+            if (sum > target) {
+                rPointer--;
+            } else if (sum < target) {
+                lPointer++;
+            } else {
+                return new int[2]{ lPointer + 1, rPointer + 1 };
+            }
+        }
+
+        return new int[2];
     }
 }
