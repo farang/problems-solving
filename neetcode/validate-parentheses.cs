3e3f85b8639ee0c93fc3c62adecab87a43438736
farang <artemiavorskyi@gmail.com>
2025-10-27 08:53:24 +0100

    validate parentheses (stack solution)

--- neetcode/validate-parentheses.cs
+++ neetcode/validate-parentheses.cs
@@ -30,3 +30,28 @@ public class Solution {
         return toClose == -1;
     }
 }
+
+// Stack solution
+public class Solution {
+    public static Dictionary<char, char> BracketsMap = new Dictionary<char, char> {
+        ['['] = ']',
+        ['('] = ')',
+        ['{'] = '}'
+    };
+
+    public bool IsValid(string s) {
+        var openBrackets = new Stack<char>();
+
+        for (var i = 0; i < s.Length; i++) {
+            if (BracketsMap.ContainsKey(s[i])) {
+                openBrackets.Push(s[i]);
+                continue;
+            }
+           if (openBrackets.Count == 0 || s[i] != BracketsMap[openBrackets.Pop()]) {
+                return false;
+           }
+        }
+
+        return openBrackets.Count == 0;
+    }
+}
