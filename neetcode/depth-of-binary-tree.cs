5d83067e19e1f167544277f3cf2dfbb45a6b8ed7
farang <artemiavorskyi@gmail.com>
2025-11-15 20:54:18 +0100

    depth of binary tree (dfs and recursive)

--- neetcode/depth-of-binary-tree.cs
+++ neetcode/depth-of-binary-tree.cs
@@ -1,3 +1,4 @@
+// iterative BFS solution
 public class Solution
 {
     public int MaxDepth(TreeNode root)
@@ -28,6 +29,55 @@ public class Solution
                     queue.Enqueue(item.right);
                 }
                 size--;
+            }
+        }
+
+        return result;
+    }
+}
+
+// recursive solution
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root is null)
+        {
+            return 0;
+        }
+
+        return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+    }
+}
+
+// iterative DFS solution
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root is null)
+        {
+            return 0;
+        }
+
+        var stack = new Stack<Tuple<TreeNode, int>>(new[] { new Tuple<TreeNode, int>(root, 1) });
+        var result = 0;
+
+        while (stack.Count > 0)
+        {
+            var item = stack.Pop();
+            var node = item.Item1;
+            var depth = item.Item2;
+
+            result = Math.Max(result, depth);
+
+            if (node.right is not null)
+            {
+                stack.Push(new Tuple<TreeNode, int>(node.right, depth + 1));
+            }
+            if (node.left is not null)
+            {
+                stack.Push(new Tuple<TreeNode, int>(node.left, depth + 1));
             }
         }
 
